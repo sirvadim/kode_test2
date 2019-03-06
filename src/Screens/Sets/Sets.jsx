75e7a818e5058d76052fc559f9f2648ea68ed3bf
sirvadim <sir.vadim2013@yandex.ru>
2019-03-06 12:16:07 +0200

    init commit

---
+++ src/Screens/Sets/Sets.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sets extends Component {
+
+  render() {
+    return (
+      <div class = "container">
+        <h1>HELLO REACT</h1>
+      </div>
+    );
+  }
+}
+
+export default Sets;
