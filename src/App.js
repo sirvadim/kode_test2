b98e2afef34edefaf0449d6b274893ab309c915b
sirvadim <sir.vadim2013@yandex.ru>
2019-03-06 13:54:16 +0200

    get data from api

--- src/App.js
+++ src/App.js
@@ -4,14 +4,41 @@ import './App.css';
 import Sets from './Screens/Sets/Sets'
 import pokemon from 'pokemontcgsdk'
 class App extends Component {
+  constructor() {
+    super();
+    this.sets = []
+    this.state = {
+      sets: this.sets
+    }
+    this.getSets()
+  }
+
+  getSets() {
+    let getSets = pokemon.set.all()
+    getSets.on('data', set => {
+      this.sets.push({
+        name: set.name,
+        logo: set.logoUrl
+      })
+    })
+    getSets.on('end', () => {
+      this.setState({
+        sets: this.sets
+      })
+    })
+  }
 
   render() {
-    pokemon.set.all()
-      .on('data', card => {
-        console.log(card.name)
-      })
+    console.log('start')
+    console.log(this.state.sets)
     return (
-      <Sets></Sets>
+      <div>
+        {
+          this.state.sets.map(set =>
+            <Sets set={set}/>
+          )
+        }
+      </div>
     );
   }
 }
--- src/Screens/Sets/Sets.jsx
+++ src/Screens/Sets/Sets.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Sets extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div class = "container">
-        <h1>HELLO REACT</h1>
+        <h1>{this.props.set.name}</h1>
+        <img src={this.props.set.logo}/>
       </div>
     );
   }
